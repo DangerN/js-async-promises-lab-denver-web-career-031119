5b3c395ea7045ffcf026f5ff96f6a13dfc89b26e
DangerN <cdneal091@gmail.com>
2019-04-26 13:26:46 -0600

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,39 @@ const questions = [
 ]
 
 let question;
+
+const qContainer = document.querySelector('.question-container')
+
+function appendQuestion(q){
+  // console.log(q.questionText)
+  // console.log(qContainer)
+  qContainer.innerHTML = q.questionText
+}
+
+function askQuestionThen(time){
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise( resolve => setTimeout( () => resolve(question), time))
+}
+
+function removeQuestion(){
+  qContainer.innerText = ""
+}
+
+function askQuestionThenRemoveQuestion(time){
+  return askQuestionThen(time).then(removeQuestion)
+}
+
+function trueAndFalseButtons(){
+  return document.querySelectorAll('.green, .red')
+}
+
+function toggleTrueAndFalseButtons(){
+  trueAndFalseButtons().forEach( button => {
+      button.classList.toggle('hide')
+  })
+}
+
+function displayQuestionOnClick(){
+  return document.querySelector('a').addEventListener('click', appendQuestion)
+}
